c541088bd663a6713e7c3a167dc771037ef74607
Morhunov Ihor <132665539+NureMorhunovIhor@users.noreply.github.com>
2024-11-24 17:29:02 +0200

    Delete Task2/atark-pzpi-22-6-morhunov-ihor-task2 directory

--- Task2/atark-pzpi-22-6-morhunov-ihor-task2/src/main/java/org/nure/atark/autoinsure/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.nure.atark.autoinsure.repository;
-
-import org.nure.atark.autoinsure.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-}
