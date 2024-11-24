4346264f5d2ca415350f08e81213a0bc0ab39891
IhorMorhunov <lapaspalma@gmail.com>
2024-11-24 18:15:38 +0200

    fix issue

--- src/main/java/org/nure/atark/autoinsure/repository/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package org.nure.atark.autoinsure.repository;
-
-import org.nure.atark.autoinsure.entity.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Integer> {
-
-}
