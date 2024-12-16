fb542230c859e0e0ef18a522c90899c75983293b
NureMorhunovIhor <lapaspalma@gmail.com>
2024-12-16 17:00:46 +0200

    idk

--- Task2/arkpz-pzpi-22-6-morhunov-ihor-task2/src/main/java/org/nure/atark/autoinsure/repository/MeasurementRepository.java
+++
@@ -1,9 +0,0 @@
-package org.nure.atark.autoinsure.repository;
-
-import org.nure.atark.autoinsure.entity.Measurement;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeasurementRepository extends JpaRepository<Measurement, Integer> {
-}
